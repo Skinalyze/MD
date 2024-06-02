d43cb75e970dd6c79b5b88247b8dd21578f366a2
Rahma Adinda Putri <putrirahmaadinda@gmail.com>
2024-06-02 07:20:05 +0700

    feat: init bottom nav

---
+++ app/src/main/java/com/example/skinalyze/DiscoveryActivity.kt
@@ -0,0 +1,35 @@
+package com.example.skinalyze
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.skinalyze.databinding.ActivityDiscoveryBinding
+
+class DiscoveryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDiscoveryBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDiscoveryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_discovery)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_product, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
