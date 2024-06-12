28517919d47b6a41607639d0862ffb6e02e2dc35
Rahma Adinda Putri <putrirahmaadinda@gmail.com>
2024-06-12 17:58:44 +0700

    feat: integrate model

---
+++ app/src/main/java/com/example/skinalyze/ImageClassifierHelper.kt
@@ -0,0 +1,104 @@
+package com.example.skinalyze
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
+import android.util.Log
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.task.core.BaseOptions
+import org.tensorflow.lite.task.core.vision.ImageProcessingOptions
+import org.tensorflow.lite.task.vision.classifier.Classifications
+import org.tensorflow.lite.task.vision.classifier.ImageClassifier
+
+class ImageClassifierHelper(
+    var threshold: Float = 0.1f,
+    var maxResults: Int = 3,
+    val modelName: String = "skin_problem_classifier.tflite",
+    val context: Context,
+    val classifierListener: ClassifierListener?
+) {
+    private var imageClassifier: ImageClassifier? = null
+
+    init {
+        setupImageClassifier()
+    }
+
+//    private fun setupImageClassifier() {
+//        val optionsBuilder = ImageClassifier.ImageClassifierOptions.builder()
+//            .setScoreThreshold(threshold)
+//            .setMaxResults(maxResults)
+//            .setImageProcessingOptions(
+//                ImageProcessingOptions.builder()
+//                    .setPreprocessNormalize(ImageProcessingOptions.NormalizeOp.SCALING, 0f, 1f)
+//                    .build()
+//            )
+//            .setNumThreads(4)
+//
+//        try {
+//            imageClassifier = ImageClassifier.createFromFileAndOptions(
+//                context,
+//                modelName,
+//                optionsBuilder.build()
+//            )
+//        } catch (e: IllegalStateException) {
+//            Log.e(TAG, e.message.toString())
+//        }
+//    }
+
+    private fun setupImageClassifier() {
+        val optionsBuilder = ImageClassifier.ImageClassifierOptions.builder()
+            .setScoreThreshold(threshold)
+            .setMaxResults(maxResults)
+        val baseOptionsBuilder = BaseOptions.builder()
+            .setNumThreads(4)
+        optionsBuilder.setBaseOptions(baseOptionsBuilder.build())
+
+        try {
+            imageClassifier = ImageClassifier.createFromFileAndOptions(
+                context,
+                modelName,
+                optionsBuilder.build()
+            )
+        } catch (e: IllegalStateException) {
+//            classifierListener?.onError(context.getString(R.string.image_classifier_failed))
+            Log.e(TAG, e.message.toString())
+        }
+    }
+
+    fun classifyStaticImage(imageUri: Uri) {
+        // TODO: mengklasifikasikan imageUri dari gambar statis.
+        if (imageClassifier == null) {
+            setupImageClassifier()
+        }
+
+        val contentResolver = context.contentResolver
+
+        val bitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val source = ImageDecoder.createSource(contentResolver, imageUri)
+            ImageDecoder.decodeBitmap(source)
+        } else {
+            MediaStore.Images.Media.getBitmap(contentResolver, imageUri)
+        }
+
+        bitmap?.let { originalBitmap ->
+            val tensorImage = TensorImage.fromBitmap(originalBitmap.copy(Bitmap.Config.ARGB_8888, true))
+
+            val results = imageClassifier?.classify(tensorImage)
+            classifierListener?.onResults(results)
+        }
+    }
+
+    interface ClassifierListener {
+        fun onError(error: String)
+        fun onResults(
+            results: List<Classifications>?
+        )
+    }
+
+    companion object {
+        private const val TAG = "ImageClassifierHelper"
+    }
+}
